dbfed6efae8698a177f3670f6d9680fce3b102a0
MultiX <ishiiro1990@gmail.com>
2024-03-27 12:30:19 +0200

    update user-status into supabase

--- lib/features/user_profile/repository/update_user_status.dart
+++ lib/features/user_profile/repository/update_user_status.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:viblify_app/core/Constant/firebase_constant.dart';
 import 'package:viblify_app/messaging/notifications.dart';
 
 class UpdateUserStatus {
+  final supabase = Supabase.instance.client;
   final _firebaseFirestore = FirebaseFirestore.instance;
   CollectionReference get _users =>
       _firebaseFirestore.collection(FirebaseConstant.usersCollection);
@@ -14,5 +16,14 @@ class UpdateUserStatus {
       'lastTimeActive': DateTime.now().millisecondsSinceEpoch.toString(),
       'notificationsToken': token,
     });
+    await supabase
+        .from(FirebaseConstant.usersCollection)
+        .update({
+          'isUserOnline': isOnline,
+          'lastTimeActive': DateTime.now().millisecondsSinceEpoch.toString(),
+          'notificationsToken': token,
+        })
+        .eq('userID', uid)
+        .single();
   }
 }
